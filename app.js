467fe14bdfcfd877bc1f8be591e05b7763fbd5af
mike-mjk <michael.kostolnik@gmail.com>
2016-11-10 13:28:02 -0800

    finished version

--- app.js
+++ app.js
@@ -16,14 +16,27 @@ var state = {
 			of America was suddenly and deliberately attacked by naval and air forces of the Empire of Japan.",
 			answers: ["Franklin D Roosevelt", "John F Kennedy", "Abraham Lincoln", "Mahatma Gandhi"],
 			correctAnswerIndex: 0
+		},
+		{
+			text: "No puppet! No Puppet! You're the puppet! No, you're the puppet!",
+			answers: ["Abraham Lincoln", "John F Kennedy", "Donald Trump", "Hillary Clinton"],
+			correctAnswerIndex: 2
+		},
+		{
+			text: "Let's dispel with this fiction that Barack Obama doesn't know\
+			 what he's doing. He knows exactly what he's doing.",
+			answers: ["Donald Trump", "Hillary Clinton", "Barack Obama", "Marco Rubio"],
+			correctAnswerIndex: 3
 		}
+
 	],
 	score: 0,
 	currentQuestionIndex: 0,
-	currentPage: 'start',
 }
 
 function renderQuestionPage(state) {
+	$(".js-form, .js-question, .js-submitAnswer, .js-questionNum").removeClass('hidden');
+	$(".js-feedback, .js-nextQuestion").addClass('hidden');
 	renderQuestionCount(state);
 	renderNumCorrect(state);
 	renderAnswers(state);
@@ -32,27 +45,61 @@ function renderQuestionPage(state) {
 	$('.js-submitAnswer').removeClass('hidden');
 }
 
+function clickNextQuestion(state) {
+	$('.js-nextQuestion').off("click").on("click", function(event) {
+		renderQuestionPage(state);
+	})
+}
+
 function renderFeedback(state) {
-	$('.js-submitAnswer').on("click", function(event) {
-		$(".js-form, .js-question, .js-submitAnswer").addClass('hidden');
+	$('.js-submitAnswer').off("click").on("click", function(event) {
+		$(".js-form, .js-question, .js-submitAnswer, .js-questionNum").addClass('hidden');
 		var checked = $("input[name='answer']:checked").val();
 		$('.js-feedback').removeClass('hidden');
+		
+		if (state.currentQuestionIndex !== state.questions.length - 1) {
+			$('.js-nextQuestion').removeClass('hidden');
+			clickNextQuestion(state);
+		}
+
+		if (state.currentQuestionIndex === state.questions.length - 1) {
+			$('.js-restart').removeClass('hidden');
+			$('.js-restart').on("click", function(event) {
+				state.currentQuestionIndex = 0;
+				state.score = 0;
+				$(this).addClass('hidden');
+				$('.js-form').removeClass('hidden');
+				$('.js-question').removeClass('hidden');
+				$('.js-questionNum').removeClass('hidden');
+				$('.js-numCorrect').removeClass('hidden');
+
+
+	renderQuestionPage(state);
+
+			});
+		}
+
+
 		if(checked == state.questions[state.currentQuestionIndex].correctAnswerIndex){
-			alert("that's right");
 			$('.js-feedback').text("That's correct!")
+			state.score = state.score + 1;
+			state.currentQuestionIndex += 1
+			renderNumCorrect(state);
 		}
 		else {
-			alert("wrong, bitch");
 			var index = (state.questions[state.currentQuestionIndex].correctAnswerIndex);
 			var correct = (state.questions[state.currentQuestionIndex].answers[index]);
 			$('.js-feedback').text("Sorry, the correct answer is " + correct);
-
+			state.currentQuestionIndex += 1;
+			renderNumCorrect(state);
 		}
+
 	});
 }
 
 function renderQuestionCount(state) {
-	$('.js-questionNum').text("Question number " + state.currentQuestionIndex + 1 + " of " + state.questions.length);
+	$('.js-questionNum').removeClass('hidden');
+	$('.js-questionNum').text("Question number " + (state.currentQuestionIndex + 1) + " of " + state.questions.length);
 }
 
 function renderNumCorrect(state) {
